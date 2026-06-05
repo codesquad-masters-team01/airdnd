10768b31521a3589f810e84571e43fc2459f6ccb
Wonjae-Song0906 <151840746+Wonjae-Song0906@users.noreply.github.com>
2026-06-05 18:16:09 +0900

    feat: [백엔드] 현재 사용자 조회 API(/api/auth/me) 임시 구현

--- apps/backend/src/main/java/com/airdnd/user/MemberController.java
+++ apps/backend/src/main/java/com/airdnd/user/MemberController.java
@@ -1,7 +1,25 @@
 package com.airdnd.user;
 
+import com.airdnd.user.dto.MemberResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
+
+    @GetMapping("/api/auth/me")
+    public ResponseEntity<MemberResponse> getCurrentUser(){
+
+        MemberResponse mockHostMember = new MemberResponse(
+                1L,
+                "host@example.com",
+                "테스트 호스트",
+                "HOST",
+                "abc",
+                "aaa",
+                false
+        );
+        return ResponseEntity.ok(mockHostMember);
+    }
 }
