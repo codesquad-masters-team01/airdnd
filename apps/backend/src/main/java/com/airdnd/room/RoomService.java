073c602553b17ded6fcf0967d99e423eef5bfe46
Wonjae-Song0906 <151840746+Wonjae-Song0906@users.noreply.github.com>
2026-06-09 18:17:29 +0900

    feat: 숙소 예약 요청에 필요한 엔티티, DTO, 컨트롤러, 서비스 구현

--- apps/backend/src/main/java/com/airdnd/room/RoomService.java
+++ apps/backend/src/main/java/com/airdnd/room/RoomService.java
@@ -54,7 +54,6 @@ public class RoomService {
 
     @Transactional(readOnly = true)
     public List<RoomResponse> getRooms() {
-
         List<RoomResponse> rooms = new ArrayList<>();
         for (Room room : roomRepository.findAllByIsActive()) {
             String imageUrl = room.getRepresentativeImageUrl();
